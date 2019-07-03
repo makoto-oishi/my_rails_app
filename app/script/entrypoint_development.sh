627eb816541187b6cc304f58db7fff67cf6837a7
Makoto Oishi <makoto.oishi.0705@gmail.com>
2019-07-03 22:55:56 +0900

    init 2

--- app/script/entrypoint_development.sh
+++ app/script/entrypoint_development.sh
@@ -2,5 +2,5 @@
 echo -------------------------------
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
-bundle exec rails s -p 3000 -b 0.0.0.0
+foreman start
 echo -------------------------------
