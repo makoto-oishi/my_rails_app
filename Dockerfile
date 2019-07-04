6544f155550be0e9456c34e5c515b9975327d818
Makoto Oishi <makoto.oishi.0705@gmail.com>
2019-07-04 22:36:18 +0900

    init2

--- Dockerfile
+++ Dockerfile
@@ -26,8 +26,6 @@ bundle install
 
 ADD app ${APP_ROOT}
 
-RUN if [ -e /app/tmp/pids/server.pid ]; then rm /app/tmp/pids/server.pid; fi
-
 EXPOSE 3000
 
 ENTRYPOINT ["/bin/sh", "-c","/app/script/entrypoint_${RAILS_ENV}.sh" ]
--- app/script/entrypoint_development.sh
+++ app/script/entrypoint_development.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 echo -------------------------------
+if [ -e /app/tmp/pids/server.pid ]; then 
+    rm -f /app/tmp/pids/server.pid; 
+fi
+
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
 foreman start
