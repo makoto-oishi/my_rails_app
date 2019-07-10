4581fd4a3c302c5a9576730c89c2ed8632333934
Makoto Oishi <makoto.oishi.0705@gmail.com>
2019-07-10 22:39:28 +0900

    add endpoint.sh with production

---
+++ app/script/entrypoint_production.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+echo -------------------------------
+if [ -e /app/tmp/pids/server.pid ]; then 
+    rm -f /app/tmp/pids/server.pid; 
+fi
+
+bundle exec rails db:migrate
+bundle exec rails assets:precompile
+bundle exec rails s -p 3000 -b 0.0.0.0
+echo -------------------------------
