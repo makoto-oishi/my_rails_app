869983658b8c4e1ccdd7ddda914fc0275321ee7c
Makoto Oishi <makoto.oishi.0705@gmail.com>
2019-07-15 20:01:50 +0900

    modify endpoint.sh

--- app/script/entrypoint_development.sh
+++ app/script/entrypoint_development.sh
@@ -4,7 +4,6 @@ if [ -e /app/tmp/pids/server.pid ]; then
     rm -f /app/tmp/pids/server.pid;
 fi
 
-bundle exec rake yarn:install
 bundle exec rails db:migrate
 foreman start
 echo -------------------------------
--- app/script/entrypoint_production.sh
+++ app/script/entrypoint_production.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-echo -------------------------------
+echo 1------------------------------
 if [ -e /app/tmp/pids/server.pid ]; then
     rm -f /app/tmp/pids/server.pid;
 fi
@@ -7,4 +7,3 @@ fi
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
 bundle exec rails s -p 3000 -b 0.0.0.0
-echo -------------------------------
